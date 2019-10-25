a57c6a2d961cdc0fb3645390fd0b179790d96dc8
jason-0419-dev <a0909082255@gmail.com>
2019-10-25 15:19:19 +0800

    ch5

--- bookstore/main/views.py
+++ bookstore/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def main(request):
+    '''
+    Show 'Hello world!' in the main page
+    '''
+    return HttpResponse('歡迎光臨本書店，來杯提神的咖啡，看本好書吧！')
